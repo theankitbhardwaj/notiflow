db59cada4a1414178c7a4ca40a69c6fc46525dca
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2026-05-25 22:57:56 +0530

    NotificationService: Events published to Kafka topic successfully

--- notification-api/src/main/java/com/notiflow/notification/service/NotificationService.java
+++ notification-api/src/main/java/com/notiflow/notification/service/NotificationService.java
@@ -1,9 +1,13 @@
 package com.notiflow.notification.service;
 
 import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import com.notiflow.notification.dto.CreateNotificationRequest;
 import com.notiflow.notification.enums.NotificationStatus;
+import com.notiflow.notification.kafka.NotificationCreatedEvent;
 import com.notiflow.notification.model.Notification;
 import com.notiflow.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,9 @@ import lombok.RequiredArgsConstructor;
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
+    private final KafkaTemplate<String, NotificationCreatedEvent> kafkaTemplate;
+    @Value("${spring.kafka.topic-names.notification-created}")
+    private String notfCreatedTopic;
 
     public Notification createNotification(CreateNotificationRequest request) {
         Notification notification = Notification.builder()
@@ -25,7 +32,13 @@ public class NotificationService {
                 .build();
 
         Notification savedNotification = notificationRepository.save(notification);
-        // Set notification properties based on the request
+
+        NotificationCreatedEvent event = new NotificationCreatedEvent(savedNotification.getId(),
+                savedNotification.getChannel(), savedNotification.getRecipient(), savedNotification.getTemplate(),
+                savedNotification.getPayload());
+
+        kafkaTemplate.send(notfCreatedTopic, savedNotification.getId().toString(), event);
+
         return savedNotification;
     }
 }
